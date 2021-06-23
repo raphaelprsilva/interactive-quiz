30e26745392f86731d0dd96d6258980070075486
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-06-23 09:43:01 -0300

    Add event listener to form

---
+++ app.js
@@ -0,0 +1,18 @@
+const form = document.querySelector('.quiz-form');
+
+const correctAnswers = ['B', 'B', 'B', 'B'];
+
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  let score = 0;
+  const userAnswers = [
+    form.inputQuestion1.value,
+    form.inputQuestion2.value,
+    form.inputQuestion3.value,
+    form.inputQuestion4.value,
+  ];
+
+  console.log(userAnswers);
+});
