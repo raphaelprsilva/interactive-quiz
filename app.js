c734317dbf4f54946adb7c79b99d184da9eb30b9
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-06-24 15:32:12 -0300

    Add scores logic

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const form = document.querySelector('.quiz-form');
+const submitButton = document.querySelector('.btn-primary');
 
 const correctAnswers = ['B', 'A', 'A', 'A', 'C', 'B'];
 const totalQuestions = correctAnswers.length;
 
+const feedbackUser = document.createElement('div');
+
+const feedbackMessageToUser = (userScoreFeedbackInfo) => {
+  const { element, classAttribute, message } = userScoreFeedbackInfo;
+
+  form.textContent = '';
+  element.setAttribute('class', classAttribute);
+  element.textContent = message;
+  form.insertAdjacentElement('afterend', element);
+};
 
 form.addEventListener('submit', (event) => {
   event.preventDefault();
@@ -17,12 +28,47 @@ form.addEventListener('submit', (event) => {
     form.sixthQuestion.value,
   ];
 
-  userAnswers.forEach((answer, index) => {
+  const computeUserScore = (answer, index) => {
     if (answer === correctAnswers[index]) {
-      const eachCorrectAnswer = Math.round((1 / totalQuestions) * 100);
+      const eachCorrectAnswer = ((1 / totalQuestions) * 100);
       userScore += eachCorrectAnswer;
     }
-  })
+  }
 
-  console.log(userScore);
+  userAnswers.forEach(computeUserScore);
+
+  const userScoreRounded = userScore.toFixed(2);
+  const isScoreGreaterThan70 = userScoreRounded >= 70;
+  const isScoreBetweem50and70 = userScoreRounded >= 50 && userScoreRounded < 70;
+
+  const successScore = {
+    element: feedbackUser,
+    classAttribute: 'my-5 alert alert-success text-center',
+    message: `Parabéns, você acertou ${userScoreRounded}% das questões! 💪🏾 🚀`,
+  };
+
+  const medianScore = {
+    element: feedbackUser,
+    classAttribute: 'my-5 alert alert-warning text-center',
+    message: `Você acertou ${userScoreRounded}% das questões.
+      Bom demais, mas dá para melhorar. 🤪`,
+  };
+
+  const badScore = {
+    element: feedbackUser,
+    classAttribute: 'my-5 alert alert-danger text-center',
+    message: `Você acertou apenas ${userScoreRounded}% das questões! 😥`,
+  };
+
+  if (isScoreGreaterThan70) {
+    feedbackMessageToUser(successScore);
+    return;
+  }
+
+  if (isScoreBetweem50and70) {
+    feedbackMessageToUser(medianScore);
+    return;
+  }
+
+  feedbackMessageToUser(badScore);
 });
