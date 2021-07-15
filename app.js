b9e2b28a47287b38d7c702f639fc92ec451e0a14
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-07-15 10:55:54 -0300

    Change score view

--- app.js
+++ app.js
@@ -5,17 +5,6 @@ const finalResult = document.querySelector('.result');
 const correctAnswers = ['B', 'A', 'A', 'A', 'C', 'B'];
 const totalQuestions = correctAnswers.length;
 
-const feedbackUser = document.createElement('div');
-
-const feedbackMessageToUser = (userScoreFeedbackInfo) => {
-  const { element, classAttribute, message } = userScoreFeedbackInfo;
-
-  form.textContent = '';
-  element.setAttribute('class', classAttribute);
-  element.textContent = message;
-  form.insertAdjacentElement('afterend', element);
-};
-
 form.addEventListener('submit', (event) => {
   event.preventDefault();
 
@@ -30,47 +19,24 @@ form.addEventListener('submit', (event) => {
     form.sixthQuestion.value,
   ];
 
-  const computeUserScore = (answer, index) => {
-    if (answer === correctAnswers[index]) {
+  userAnswers.forEach((userAnswer, index) => {
+    if (userAnswer === correctAnswers[index]) {
       const eachCorrectAnswer = (1 / totalQuestions) * 100;
       userScore += eachCorrectAnswer;
     }
-  };
+  });
 
-  userAnswers.forEach(computeUserScore);
+  scrollTo(0, 0);
+  finalResult.classList.remove('d-none');
 
-  const userScoreRounded = userScore.toFixed(2);
-  const isScoreGreaterThan70 = userScoreRounded >= 70;
-  const isScoreBetweem50and70 = userScoreRounded >= 50 && userScoreRounded < 70;
+  let counter = 0;
 
-  const successScore = {
-    element: feedbackUser,
-    classAttribute: 'my-5 alert alert-success text-center',
-    message: `Parabéns, você acertou ${userScoreRounded}% das questões! 💪🏾 🚀`,
-  };
+  const timer = setInterval(() => {
+    if (counter === Math.round(userScore)) {
+      clearInterval(timer);
+    }
 
-  const medianScore = {
-    element: feedbackUser,
-    classAttribute: 'my-5 alert alert-warning text-center',
-    message: `Você acertou ${userScoreRounded}% das questões.
-      Bom demais, mas dá para melhorar. 🤪`,
-  };
-
-  const badScore = {
-    element: feedbackUser,
-    classAttribute: 'my-5 alert alert-danger text-center',
-    message: `Você acertou apenas ${userScoreRounded}% das questões! 😥`,
-  };
-
-  if (isScoreGreaterThan70) {
-    feedbackMessageToUser(successScore);
-    return;
-  }
-
-  if (isScoreBetweem50and70) {
-    feedbackMessageToUser(medianScore);
-    return;
-  }
-
-  feedbackMessageToUser(badScore);
+    finalResult.querySelector('span').textContent = `${counter}%`;
+    counter++;
+  }, 100);
 });
